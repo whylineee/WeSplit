b9026ad1fc93f1d06635c6dfcce5a92ae05cd9ed
whylineee <msavin901001@gmail.com>
2026-01-26 22:13:40 +0200

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Marko Savyn on 26.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
