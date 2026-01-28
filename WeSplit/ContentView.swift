b955f4906f9f5d81b56b90cf21d9f801fba7e22c
whylineee <msavin901001@gmail.com>
2026-01-28 18:56:18 +0200

    + new button, new functio, new text

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,17 +8,56 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 20
+    
+    let tipPercentages = [10, 15, 20, 25, 0]
+    
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            Form {
+                Section {
+                    TextField("Amount", value: $checkAmount, format: .currency(code:
+                        Locale.current.currency?.identifier ?? "USD"))
+                    .keyboardType(.decimalPad)
+                    
+                    Picker("Number of people", selection: $numberOfPeople) {
+                        ForEach(1..<21) {
+                            Text("\($0) people")
+                        }
+                    }
+                    .pickerStyle(.navigationLink)
+                    
+                }
+                
+                Section("How much do you want to tip?") {
+                    
+                    Picker("Tip percentage", selection: $tipPercentage)  {
+                        ForEach(tipPercentages, id: \.self) {
+                            Text($0, format: .percent)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                }
+                
+                Section {
+                    Text(checkAmount, format: .currency(code:
+                    Locale.current.currency?.identifier ?? "USD"))
+                }
+            }
+            .navigationTitle(Text("WeSplit"))
         }
-        .padding()
+        
+        
+        
+        
     }
+    
+    
 }
-
 #Preview {
     ContentView()
 }
+
