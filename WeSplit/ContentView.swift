0ce186d4d3fd48669c162ccc7e4e324dd589e5da
whylineee <msavin901001@gmail.com>
2026-01-30 20:36:37 +0200

    test

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -47,7 +47,7 @@ struct ContentView: View {
                     Locale.current.currency?.identifier ?? "USD"))
                 }
             }
-            .navigationTitle(Text("WeSplitt"))
+            .navigationTitle(Text("WeSplit"))
         }
         
         
